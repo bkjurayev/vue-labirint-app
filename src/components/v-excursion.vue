ad6419a1e020f31ccf4adabc653760c298b2e5a5
bkjurayev <jurayev_94@bk.ru>
2022-08-21 11:20:29 +0500

    added scss codes

--- src/components/v-excursion.vue
+++ src/components/v-excursion.vue
@@ -14,11 +14,13 @@
             <input class="excursion__form__input" type="text" placeholder="Ism">
             <input class="excursion__form__input" type="text" placeholder="Familiya">
             <input class="excursion__form__input" type="number" placeholder="Telefon raqamingiz">
-            <textarea  class="excursion__form__input" rows="6" cols="20" placeholder="Fikr va takliflar"></textarea>
+            <textarea  class="excursion__form__input textarea" rows="6" cols="20" placeholder="Fikr va takliflar"></textarea>
             <button class="excursion__form__btn _btn">Jo'natish</button>
             <p class="excursion__form__info">Ma'lumotlaringiz 3-shaxslarga berilmaydi</p>
         </form>
-        <div class="excursion__img"></div>
+        <div class="excursion__img">
+          <img src="../assets/images/labirint.png" alt="Labirint-reception">
+        </div>
       </div>
     </div>
   </section>
